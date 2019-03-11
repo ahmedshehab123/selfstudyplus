ed3ec4fdf2482647ddbc7e13b11ec6d0a38e4bdb
ahmed <ahmedshehab1000@gmail.com>
2019-03-12 01:07:46 +0200

    init commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,118 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-
+import '../assets/styles.css'
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header >
+    <div id="SITE_HEADERinlineContent" className="style-ini6ejptinlineContent">
+      <nav id="comp-ihjax7ir" className="hidden-during-prewarmup style-izhkmab2"
+           style={{left: '0px', width: '978px', position: 'absolute', top: '86px', height: '42px', 'overflow-x': 'visible'}}
+           data-menuborder-y="0" data-menubtn-border="0" data-ribbon-els="0" data-label-pad="0" data-ribbon-extra="0"
+           data-drophposition="" data-dropalign="center" dir="ltr" data-state="center notMobile"
+           data-dropmode="dropDown">
+        <ul style={{'text-align':'center'}} aria-label="Site navigation" role="navigation" id="comp-ihjax7iritemsContainer"
+            className="style-izhkmab2itemsContainer">
+          <li data-direction="ltr" data-listposition="left" data-data-id="dataItem-ihjdb7ac"
+              className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir0"
+              data-original-gap-between-text-and-btn="10" aria-hidden="false"
+              style={{width: '197px', height: '42px', position: 'relative', 'box-sizing': 'border-box', overflow: 'visible'}}><a
+            role="button" tabIndex="0" aria-haspopup="false" data-listposition="left"
+            href="https://www.selfstudy.plus/features" target="_self" id="comp-ihjax7ir0linkElement"
+            className="style-izhkmab2repeaterButtonlinkElement">
+            <div className="style-izhkmab2repeaterButton_gapper">
+              <div style={{'text-align':'center'}} id="comp-ihjax7ir0bg" className="style-izhkmab2repeaterButtonbg"><p
+                style={{"text-align": "center", 'line-height': '42px'}} id="comp-ihjax7ir0label"
+                className="style-izhkmab2repeaterButtonlabel">FEATURES</p></div>
+            </div>
+          </a></li>
+          <li data-direction="ltr" data-listposition="center" data-data-id="dataItem-ivcxmsvu"
+              className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir1"
+              data-original-gap-between-text-and-btn="11" aria-hidden="false"
+              style={{width: "285px", height: '42px', position: 'relative', 'box-sizing': 'border-box', overflow: 'visible'}}><a
+            role="button" tabIndex="0" aria-haspopup="false" data-listposition="center"
+            href="https://www.selfstudy.plus/anesthesiologists" target="_self" id="comp-ihjax7ir1linkElement"
+            className="style-izhkmab2repeaterButtonlinkElement">
+            <div className="style-izhkmab2repeaterButton_gapper">
+              <div style={{'text-align':'center'}} id="comp-ihjax7ir1bg" className="style-izhkmab2repeaterButtonbg"><p
+                style={{'text-align': 'center', 'line-height': '42px'}} id="comp-ihjax7ir1label"
+                className="style-izhkmab2repeaterButtonlabel">ANESTHESIOLOGISTS</p></div>
+            </div>
+          </a></li>
+          <li data-direction="ltr" data-listposition="center" data-data-id="dataItem-iv8h0mo6"
+              className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir2"
+              data-original-gap-between-text-and-btn="11" aria-hidden="false"
+              style={{width: '205px' ,height: '42px', position: 'relative', 'box-sizing': 'border-box', overflow: 'visible'}}><a
+            role="button" tabIndex="0" aria-haspopup="false" data-listposition="center"
+            href="https://www.selfstudy.plus/residents-1" target="_self" id="comp-ihjax7ir2linkElement"
+            className="style-izhkmab2repeaterButtonlinkElement">
+            <div className="style-izhkmab2repeaterButton_gapper">
+              <div style={{'text-align':'center'}} id="comp-ihjax7ir2bg" className="style-izhkmab2repeaterButtonbg"><p
+                style={{'text-align': 'center', 'line-height': '42px'}} id="comp-ihjax7ir2label"
+                className="style-izhkmab2repeaterButtonlabel">RESIDENTS</p></div>
+            </div>
+          </a></li>
+          <li data-direction="ltr" data-listposition="right" data-data-id="dataItem-im6vbn2h"
+              className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir3"
+              data-original-gap-between-text-and-btn="10" aria-hidden="false"
+              style={{width: '291px', height: '42px', position: 'relative', 'box-sizing': 'border-box', overflow: 'visible'}}><a
+            role="button" tabIndex="0" aria-haspopup="false" data-listposition="right"
+            href="https://www.selfstudy.plus/program-directors" target="_self" id="comp-ihjax7ir3linkElement"
+            className="style-izhkmab2repeaterButtonlinkElement">
+            <div className="style-izhkmab2repeaterButton_gapper">
+              <div style={{'text-align':'center'}} id="comp-ihjax7ir3bg" className="style-izhkmab2repeaterButtonbg"><p
+                style={{'text-align': 'center', 'line-height': '42px'}} id="comp-ihjax7ir3label"
+                className="style-izhkmab2repeaterButtonlabel">PROGRAM DIRECTORS</p></div>
+            </div>
+          </a></li>
+          <li data-listposition="right" className="style-izhkmab2repeaterButton"
+              data-state="menu  idle header notMobile" id="comp-ihjax7ir__more__"
+              data-original-gap-between-text-and-btn="11" aria-hidden="true"
+              style={{height: '0px', overflow: 'hidden', position: 'absolute'}}><a role="button" tabIndex="-1"
+                                                                            aria-haspopup="true"
+                                                                            data-listposition="right"
+                                                                            id="comp-ihjax7ir__more__linkElement"
+                                                                            className="style-izhkmab2repeaterButtonlinkElement">
+            <div className="style-izhkmab2repeaterButton_gapper">
+              <div style={{'text-align':'center'}} id="comp-ihjax7ir__more__bg" className="style-izhkmab2repeaterButtonbg"><p
+                style={{'text-align':'center'}} id="comp-ihjax7ir__more__label"
+                className="style-izhkmab2repeaterButtonlabel">More</p></div>
+            </div>
+          </a></li>
+        </ul>
+        <div id="comp-ihjax7irmoreButton" className="style-izhkmab2moreButton"></div>
+        <nav style={{visibility: 'hidden', left: '0px', right: 'auto', bottom: 'auto'}} data-drophposition=""
+             data-dropalign="center" id="comp-ihjax7irdropWrapper" className="style-izhkmab2dropWrapper">
+          <ul style={{visibility: 'hidden', left: '0px', right: 'auto'}} id="comp-ihjax7irmoreContainer"
+              className="style-izhkmab2moreContainer"></ul>
+        </nav>
+      </nav>
+
+
+      <div style={{left: '39px', position: "absolute", top: '22px', width: '305px', height: '56px'}} data-exact-height="56"
+           data-content-padding-horizontal="0" data-content-padding-vertical="0" title="" className="wp2"
+           id="comp-ini0mw3r"><a style={{cursor: 'pointer', width: '305px', height: '56px'}} href="https://www.selfstudy.plus/"
+                                 target="_blank" data-content="https://www.selfstudy.plus/" data-type="external"
+                                 id="comp-ini0mw3rlink" className="wp2link">
+        <div style={{top: '0px', left: '0px', position: 'relative', width: '305px', height: '56px'}} data-style=""
+             className="wp2img" id="comp-ini0mw3rimg"><img id="comp-ini0mw3rimgimage"
+                                                           style={{'object-position': '50% 50%', width: '305px', height: '56px' ,'object-fit': 'cover'}}
+                                                           alt="" data-type="image"
+                                                           src="https://static.wixstatic.com/media/ad5c84_30ce682f6f5c49f1b78a3391c99810cf.png/v1/fill/w_305,h_56,al_c,lg_1,q_80/ad5c84_30ce682f6f5c49f1b78a3391c99810cf.webp"></img>
+        </div>
+      </a></div>
+      <div style={{left: '433px', position: 'absolute', top: '4px', width: '545px', height: '74px'}} data-exact-height="74"
+           data-content-padding-horizontal="0" data-content-padding-vertical="0" title="" className="wp2"
+           id="comp-j145e6xn"><a style={{cursor: 'pointer', width: '545px', height: '74px'}}
+                                 href="http://www.na-application.selfstudy.plus/" target="_blank"
+                                 data-content="http://www.na-application.selfstudy.plus/" data-type="external"
+                                 id="comp-j145e6xnlink" className="wp2link">
+        <div style={{top: '0px', left: '0px', position: 'relative', width: '545px', height: '74px'}} data-style=""
+             className="wp2img" id="comp-j145e6xnimg"><img id="comp-j145e6xnimgimage"
+                                                           style={{'object-position': '50% 50%', width: '545px', height: '74px', 'object-fit': 'cover'}}
+                                                           alt="" data-type="image"
+                                                           src="https://static.wixstatic.com/media/ad5c84_e7f5853123c444f1aed1282a4764fa38~mv2.png/v1/fill/w_545,h_74,al_c,q_80,usm_0.66_1.00_0.01/ad5c84_e7f5853123c444f1aed1282a4764fa38~mv2.webp"></img>
+        </div>
+      </a></div>
     </div>
   </header>
 )
