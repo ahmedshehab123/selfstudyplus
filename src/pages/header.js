f765504db2297e3cb6beb940d46a00c513f2633c
ahmed <ahmedshehab1000@gmail.com>
2019-03-13 03:18:24 +0200

    convert all wix pages to gatsby component

---
+++ src/pages/header.js
@@ -0,0 +1,181 @@
+
+import React from "react"
+import "../assets/css/styles.css"
+import { Link } from "gatsby"
+
+class NewHeader extends React.Component{
+  render() {
+    return (
+  <div>
+
+    <header data-is-mobile="false" data-state="fixedPosition" data-site-width={980} data-header-top={0} style={{
+      position: "fixed",
+      marginTop: "0px",
+      left: "0px",
+      marginLeft: "0px",
+      width: "100%",
+      minWidth: "980px",
+      zIndex: 50,
+      top: "0px",
+      height: "143px",
+    }} className="style-ini6ejpt" id="SITE_HEADER">
+      <div style={{ left: 0, width: "100%" }} id="SITE_HEADERscreenWidthBackground"
+           className="style-ini6ejptscreenWidthBackground">
+        <div className="style-ini6ejpt_bg"/>
+      </div>
+      <div id="SITE_HEADERcenteredContent" className="style-ini6ejptcenteredContent">
+        <div style={{ marginLeft: "calc((100% - 980px) / 2)", width: "980px" }} id="SITE_HEADERbg"
+             className="style-ini6ejptbg">
+          <div className="style-ini6ejpt_bg-center"/>
+        </div>
+        <div id="SITE_HEADERinlineContent" className="style-ini6ejptinlineContent">
+          <style id="SITE_HEADER-content-area-styles"
+                 dangerouslySetInnerHTML={{ __html: "\n    \n#comp-ihjax7ir {\n    margin-left: calc((100% - 980px) * 0.5);\n}\n\n#comp-ini0mw3r {\n    margin-left: calc((100% - 980px) * 0.5);\n}\n\n#comp-j145e6xn {\n    margin-left: calc((100% - 980px) * 0.5);\n}" }}/>
+          <nav id="comp-ihjax7ir" className="hidden-during-prewarmup style-izhkmab2" style={{
+            left: "0px",
+            width: "978px",
+            position: "absolute",
+            top: "86px",
+            height: "42px",
+            overflowX: "visible",
+          }} data-menuborder-y={0} data-menubtn-border={0} data-ribbon-els={0} data-label-pad={0} data-ribbon-extra={0}
+               data-drophposition data-dropalign="center" dir="ltr" data-state="center notMobile"
+               data-dropmode="dropDown">
+            <ul style={{ textAlign: "center" }} aria-label="Site navigation" role="navigation"
+                id="comp-ihjax7iritemsContainer" className="style-izhkmab2itemsContainer">
+              <li data-direction="ltr" data-listposition="left" data-data-id="dataItem-ihjdb7ac"
+                  className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir0"
+                  data-original-gap-between-text-and-btn={10} aria-hidden="false" style={{
+                width: "197px",
+                height: "42px",
+                position: "relative",
+                boxSizing: "border-box",
+                overflow: "visible",
+              }}><Link role="button" tabIndex={0} aria-haspopup="false" data-listposition="left"
+                       to="component/features"  id="comp-ihjax7ir0linkElement"
+                       className="style-izhkmab2repeaterButtonlinkElement">
+                <div className="style-izhkmab2repeaterButton_gapper">
+                  <div style={{ textAlign: "center" }} id="comp-ihjax7ir0bg" className="style-izhkmab2repeaterButtonbg"><p
+                    style={{ textAlign: "center", lineHeight: "42px" }} id="comp-ihjax7ir0label"
+                    className="style-izhkmab2repeaterButtonlabel">FEATURES</p></div>
+                </div>
+              </Link></li>
+              <li data-direction="ltr" data-listposition="center" data-data-id="dataItem-ivcxmsvu"
+                  className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir1"
+                  data-original-gap-between-text-and-btn={11} aria-hidden="false" style={{
+                width: "285px",
+                height: "42px",
+                position: "relative",
+                boxSizing: "border-box",
+                overflow: "visible",
+              }}><Link role="button" tabIndex={0} aria-haspopup="false" data-listposition="center"
+                    to="component/anesthesiologists" target="_self" id="comp-ihjax7ir1linkElement"
+                    className="style-izhkmab2repeaterButtonlinkElement">
+                <div className="style-izhkmab2repeaterButton_gapper">
+                  <div style={{ textAlign: "center" }} id="comp-ihjax7ir1bg" className="style-izhkmab2repeaterButtonbg"><p
+                    style={{ textAlign: "center", lineHeight: "42px" }} id="comp-ihjax7ir1label"
+                    className="style-izhkmab2repeaterButtonlabel">ANESTHESIOLOGISTS</p></div>
+                </div>
+              </Link></li>
+              <li data-direction="ltr" data-listposition="center" data-data-id="dataItem-iv8h0mo6"
+                  className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir2"
+                  data-original-gap-between-text-and-btn={11} aria-hidden="false" style={{
+                width: "205px",
+                height: "42px",
+                position: "relative",
+                boxSizing: "border-box",
+                overflow: "visible",
+              }}><Link role="button" tabIndex={0} aria-haspopup="false" data-listposition="center"
+                    to="component/residents-1" target="_self" id="comp-ihjax7ir2linkElement"
+                    className="style-izhkmab2repeaterButtonlinkElement">
+                <div className="style-izhkmab2repeaterButton_gapper">
+                  <div style={{ textAlign: "center" }} id="comp-ihjax7ir2bg" className="style-izhkmab2repeaterButtonbg"><p
+                    style={{ textAlign: "center", lineHeight: "42px" }} id="comp-ihjax7ir2label"
+                    className="style-izhkmab2repeaterButtonlabel">RESIDENTS</p></div>
+                </div>
+              </Link></li>
+              <li data-direction="ltr" data-listposition="right" data-data-id="dataItem-im6vbn2h"
+                  className="style-izhkmab2repeaterButton" data-state="menu  idle link notMobile" id="comp-ihjax7ir3"
+                  data-original-gap-between-text-and-btn={10} aria-hidden="false" style={{
+                width: "291px",
+                height: "42px",
+                position: "relative",
+                boxSizing: "border-box",
+                overflow: "visible",
+              }}><Link role="button" tabIndex={0} aria-haspopup="false" data-listposition="right"
+                    to="component/program-directors" target="_self" id="comp-ihjax7ir3linkElement"
+                    className="style-izhkmab2repeaterButtonlinkElement">
+                <div className="style-izhkmab2repeaterButton_gapper">
+                  <div style={{ textAlign: "center" }} id="comp-ihjax7ir3bg" className="style-izhkmab2repeaterButtonbg"><p
+                    style={{ textAlign: "center", lineHeight: "42px" }} id="comp-ihjax7ir3label"
+                    className="style-izhkmab2repeaterButtonlabel">PROGRAM DIRECTORS</p></div>
+                </div>
+              </Link></li>
+              <li data-listposition="right" className="style-izhkmab2repeaterButton"
+                  data-state="menu  idle header notMobile" id="comp-ihjax7ir__more__"
+                  data-original-gap-between-text-and-btn={11} aria-hidden="true"
+                  style={{ height: "0px", overflow: "hidden", position: "absolute" }}><a role="button" tabIndex={-1}
+                                                                                         aria-haspopup="true"
+                                                                                         data-listposition="right"
+                                                                                         id="comp-ihjax7ir__more__linkElement"
+                                                                                         className="style-izhkmab2repeaterButtonlinkElement">
+                <div className="style-izhkmab2repeaterButton_gapper">
+                  <div style={{ textAlign: "center" }} id="comp-ihjax7ir__more__bg"
+                       className="style-izhkmab2repeaterButtonbg"><p style={{ textAlign: "center" }}
+                                                                     id="comp-ihjax7ir__more__label"
+                                                                     className="style-izhkmab2repeaterButtonlabel">More</p>
+                  </div>
+                </div>
+              </a></li>
+            </ul>
+            <div id="comp-ihjax7irmoreButton" className="style-izhkmab2moreButton"/>
+            <nav style={{ visibility: "hidden", left: "auto", right: "0px", bottom: "auto" }} data-drophposition
+                 data-dropalign="center" id="comp-ihjax7irdropWrapper" className="style-izhkmab2dropWrapper">
+              <ul style={{ visibility: "hidden", left: "auto", right: "0px" }} id="comp-ihjax7irmoreContainer"
+                  className="style-izhkmab2moreContainer"/>
+            </nav>
+          </nav>
+          <div style={{ left: "39px", position: "absolute", top: "22px", width: "305px", height: "56px" }}
+               data-exact-height={56} data-content-padding-horizontal={0} data-content-padding-vertical={0} title
+               className="wp2" id="comp-ini0mw3r"><Link style={{ cursor: "pointer", width: "305px", height: "56px" }}
+                                                     to="/" target="_blank"
+                                                     data-content="/" data-type="external"
+                                                     id="comp-ini0mw3rlink" className="wp2link">
+            <div style={{ top: "0px", left: "0px", position: "relative", width: "305px", height: "56px" }} data-style
+                 className="wp2img" id="comp-ini0mw3rimg"><img id="comp-ini0mw3rimgimage" style={{
+              objectPosition: "50% 50%",
+              width: "305px",
+              height: "56px",
+              objectFit: "cover",
+            }} alt data-type="image"
+                                                               src={require('../assets/img/img20.webp')}/>
+            </div>
+          </Link></div>
+          <div style={{ left: "433px", position: "absolute", top: "4px", width: "545px", height: "74px" }}
+               data-exact-height={74} data-content-padding-horizontal={0} data-content-padding-vertical={0} title
+               className="wp2" id="comp-j145e6xn"><Link style={{ cursor: "pointer", width: "545px", height: "74px" }}
+                                                     to='/'
+                                                     data-content="http://www.na-application.selfstudy.plus/"
+                                                     data-type="external" id="comp-j145e6xnlink" className="wp2link">
+            <div style={{ top: "0px", left: "0px", position: "relative", width: "545px", height: "74px" }} data-style
+                 className="wp2img" id="comp-j145e6xnimg"><img id="comp-j145e6xnimgimage" style={{
+              objectPosition: "50% 50%",
+              width: "545px",
+              height: "74px",
+              objectFit: "cover",
+            }} alt data-type="image"
+                                                               src={require('../assets/img/img21.webp')}/>
+            </div>
+          </Link></div>
+        </div>
+      </div>
+    </header>
+  </div>
+
+    )
+
+  }
+}
+
+
+export default NewHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,13 @@ import "../assets/css/styles.css"
 import "../assets/css/viewer.min.css"
 
 
-import Header from "../components/header"
-import Home from "../components/home"
+import Home from "./component/home"
 
-import Footer from "../components/footer"
+
 
 const IndexPage = () => (
   <div>
-    <Header/>
     <Home/>
-{/*
-    <Footer/>
-*/}
-
   </div>
 )
 
